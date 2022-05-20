fc83816d2f557c97fca66ff5d0fcec87bf52a8d9
deSelbyPsyche <jackreid2@btinternet.com>
2022-05-20 17:06:55 +0100

    Adds tutorial on how to convert a form from class based to hooks.

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { Container } from "reactstrap";
+import FunctionBasedForm from "./components/FunctionBasedForm";
+import Logo from "./assets/alligator-logo2.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <img src={Logo} alt="alligator.io logo" width="200" />
+        <Container>
+          <FunctionBasedForm />
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
